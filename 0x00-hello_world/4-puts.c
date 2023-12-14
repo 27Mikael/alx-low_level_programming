48468059b1c15ade651277fe1e53433abc23e882
Michael Kanku <micheal.kanku27@gmail.com>
2023-12-14 17:25:11 +0200

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -prints the phrase "Programming is like building a multilingual 
+ * main -prints the phrase "Programming is like building a multilingual
 puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
@@ -10,3 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
