b619e1bf5ecc8c32cec637e007cf70a8ef7b4249
Michael Kanku <micheal.kanku27@gmail.com>
2023-12-14 17:10:01 +0200

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main prints out the phrase with proper grammar
+ *,but the outocome is a piece of art
+ *return 0 if exited properly
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outocome is a piece of art,\n");
+	return (0);
+}
