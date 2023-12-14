f9eba74e76c75fbef1ce681760c7d542f13a1520
Michael Kanku <micheal.kanku27@gmail.com>
2023-12-14 17:44:55 +0200

    errors fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main prints out the phrase with proper grammar
- *,but the outocome is a piece of art
- *return 0 if exited properly
+ *main - prints out the phrase with proper grammar,
+ *but the outocome is a piece of art
+ *Return: 0 if exited properly
  */
 
 int main(void)
