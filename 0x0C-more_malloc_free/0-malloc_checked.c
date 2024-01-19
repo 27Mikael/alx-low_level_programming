e38b7a074c843efe4437c1916f8c204f1131afed
27Mikael <vitalybodgan@gmail.com>
2024-01-19 17:56:34 +0200

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of byte allocated
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned in b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+
+	if (pointer == NULL)
+		exit(98);
+
+	return (pointer);
+}
