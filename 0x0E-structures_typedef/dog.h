afa6b699a89ac4213914f37089fddc772b49cabb
27Mikael <vitalybodgan@gmail.com>
2024-01-24 01:54:55 +0200

    dog.h fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  * @age: dogs age
  * @owner: name of dogs owner
  */
-
 struct dog
 {
 	char  *name;
@@ -17,7 +16,7 @@ struct dog
 
 
 /**
- * dog_t -typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
