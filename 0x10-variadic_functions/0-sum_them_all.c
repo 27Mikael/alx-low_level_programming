2075a8f6d72ceddd4b00a32f0db81eead4f924da
27Mikael <vitalybodgan@gmail.com>
2024-01-25 18:23:41 +0200

    first push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,12 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+        unsigned int i;
+	int sum = 0;
+	va_list list;
+
         if (n == 0)
                 return(0);
-	unsigned int i;
-	int sum = 0;
-	
-	va_list list;
 
 	va_start(list, n);
 
