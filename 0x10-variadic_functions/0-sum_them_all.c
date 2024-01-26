a382eb41e6cc826dd1e69a584bcc4e84d3215d0c
27Mikael <vitalybodgan@gmail.com>
2024-01-26 13:44:31 +0200

    bux fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,23 +3,25 @@
 
 /**
  * sum_them_all - sums all the parameters
- *@n:  number of arguments passed to the function
- *Return: sum of all parameters
+ * @n: number of arguments passed to the function
+ * Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-        if (n == 0)
-                return(0);
-        
-        unsigned int i;
+	unsigned int i;
 	int sum = 0;
 	va_list list;
+
 	va_start(list, n);
 
+	if (n == 0)
+		return (0);
+
 	for (i = 0; i < n; i++)
-	        sum += va_arg(list, int);
-	
+		sum += va_arg(list, int);
+
 	va_end(list);
 
 	return (sum);
 }
+
