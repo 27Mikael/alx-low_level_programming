3f40d3c55d2aefe387823642a59d4b45ab551b0f
27Mikael <vitalybodgan@gmail.com>
2024-01-26 15:24:35 +0200

    fixed 1-p_n and 2-p_s

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,28 +4,26 @@
 
 /**
  * print_numbers - prints numbers and a separator
- * @separator: string to be printed between numbers
+ * @separator: string to be printed between numbers (if not NULL)
  * @n: the number of int passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list list;
+	unsigned int i;
+	va_list list;
 
-    va_start(list, n);
+	va_start(list, n);
 
-    for (i = 0; i < n; i++)
-    {
-        if (!separator)
-            printf("%d", va_arg(list, int));
-        else if (separator && i == 0)
-            printf("%d", va_arg(list, int));
-        else
-            printf("%s%d", separator, va_arg(list, int));
-    }
+	for (i = 0; i < n; i++)
+	{
+		if (!separator || i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
+	}
 
-    va_end(list);
+	va_end(list);
 
-    printf("\n");
+	printf("\n");
 }
 
